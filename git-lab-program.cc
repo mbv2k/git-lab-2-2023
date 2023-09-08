e247c0efb74a782592df4b0d3a4c19de580b5a11
Mubarak Al Hamdan <ma585721@ohio.edu>
2023-09-08 18:52:16 -0400

    done

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Mubarak Alhamdan
+ *        Date: 9/8/2023
+ * Description: Lab 2 assignment
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
